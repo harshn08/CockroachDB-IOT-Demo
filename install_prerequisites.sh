292fc4e42453375cb1fb0a437a99f3b820e5c4e5
Harsh Shah <hshah@crlMBP-C02GH1GMMD6TNjIw.local>
2024-02-16 13:37:03 -0800

    adding images for readme file

---
+++ install_prerequisites.sh
@@ -0,0 +1,11 @@
+sudo yum install -y git
+sudo yum install -y npm
+sudo yum install -y nodejs
+sudo npm install express
+sudo npm install ejs
+sudo npm install pg
+sudo yum install -y python3
+sudo yum install -y python3-pip
+sudo pip3 install psycopg2-binary
+sudo pip3 install faker
+sudo pip3 install datetime
